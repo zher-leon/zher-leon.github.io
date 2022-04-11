50b941c4889602979507d04edc9fbb974098a05d
zher_leon <zherleon@126.com>
2022-04-11 22:38:53 +0800

    去掉token

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,4 @@
 const githubConfig = {
-  token:'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',
   login: "zher-leon",
   repo: "github.io.zher-leon", //项目名称
   branch: "Notes", //项目分支
@@ -7,5 +6,4 @@ const githubConfig = {
   deployment_ciphertext:
     "nbrQ3EO7u+6g8SWPc6X5NTZvS5kccFvGnaYlO9hzGpyzIUUJhc7dzmJcOQtFDd8+", //部署令牌密文
 };
-// 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
 export { githubConfig };
